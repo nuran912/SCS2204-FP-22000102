37d6383c0799e286d9faca8c954e24d16d4b341a
nuran912 <159169340+nuran912@users.noreply.github.com>
2024-06-25 02:11:27 +0530

    Delete Q1.scala

--- Q1.scala
+++
@@ -1,8 +0,0 @@
-object area{
-    def diskArea(r:Double):Double=r*r*3.14 
-
-    def main(args: Array[String]):Unit={
-        val area = diskArea(5)
-        printf("%s %.2f","Area of disk:",area)
-    }
-}
