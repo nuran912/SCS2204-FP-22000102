c55795b7b04a90c062ac6ce12feccd7a3473581a
nuran912 <159169340+nuran912@users.noreply.github.com>
2024-06-25 02:12:08 +0530

    Delete Q3.scala

--- Q3.scala
+++
@@ -1,8 +0,0 @@
-object sphere{
-    def volume(r: Double) = 4/3*3.14*r*r*r
-
-    def main(args: Array[String]):Unit={
-        val vol = volume(5)
-        printf("%s %.2f", "Volume of the sphere is: ", vol)
-    }
-}
